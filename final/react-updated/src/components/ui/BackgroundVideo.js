f63f3efdeffbd40093ea0e60d019095446f7d221
Ifunanya <ify.zubby@gmail.com>
2023-09-11 16:48:26 +0100

    Push 1

--- final/react-updated/src/components/ui/BackgroundVideo.js
+++ final/react-updated/src/components/ui/BackgroundVideo.js
@@ -1,9 +1,11 @@
+import React from "react"
+import IntroVideo from "./../../Video/IntroVideo.mp4"
 
 
 const BackgroundVideo = () => {
     return (
         <div className = {"video-container"} >
-            <video> </video>
+            <video src={IntroVideo} autoPlay loop muted />
         </div>
     )
 }
