ae4abcb1a206cdcee46c6192efcfc6d3c3a6003a
Ifunanya <ify.zubby@gmail.com>
2023-09-11 14:31:47 +0100

    Push 1

--- final/react-updated/src/pages/Home/Home.js
+++ final/react-updated/src/pages/Home/Home.js
@@ -1,7 +1,11 @@
 import Layout from "../../components/ui/Layout";
+import { useState,useEffect,Children } from "react";
+
 const Home = ({ children }) => {
     return (
         <Layout>
+            <h1> Flavour Match </h1>
+            <div class="element"></div>
             <div>Some awesome screen that looks awe inspiring</div>
             {children}
         </Layout>
