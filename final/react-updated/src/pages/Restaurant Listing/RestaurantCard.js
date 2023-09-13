f3e68f6bc078362da2941621ee7975628498f06a
Faiza Jibril <jayfaiza04@gmail.com>
2023-09-13 17:56:07 +0100

    Restaurant Card 1

--- final/react-updated/src/pages/Restaurant Listing/RestaurantCard.js
+++ final/react-updated/src/pages/Restaurant Listing/RestaurantCard.js
@@ -1,54 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import Card from 'react-bootstrap/Card';
 
-const dummyData = [
-  {
-    name: 'Sample Restaurant 1',
-    description: 'A cozy place to enjoy delicious food.',
-    location: '123 Main Street, City',
-    price: '$$',
-    picture: 'https://via.placeholder.com/150', // Placeholder image URL
-  },
-  {
-    name: 'Sample Restaurant 2',
-    description: 'Another great place for dining.',
-    location: '456 Elm Street, Town',
-    price: '$$$',
-    picture: 'https://via.placeholder.com/150', // Placeholder image URL
-  },
-  // Add more dummy restaurant data as needed
-];
-
 function RestaurantCard() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const { name, description, location, price, picture } = dummyData[currentIndex];
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    async function fetchRestaurantData() {
+      try {
+        const response = await fetch('http://localhost:9003/restaurant'); // Use quotes around the URL
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setRestaurants(data); // Assuming the API returns an array of restaurants
+      } catch (error) {
+        console.error('Error fetching restaurant data:', error);
+      }
+    }
+    fetchRestaurantData();
+  }, []); // Make sure to pass an empty dependency array to fetch data only once
+
+  const { id, name, priceRange, reviews, photoUrl, address } =
+    restaurants[currentIndex] || {};
 
   // Define swipe handlers
   const handlers = useSwipeable({
     onSwipedLeft: () => {
       // Handle left swipe (e.g., dislike the restaurant)
       setCurrentIndex((prevIndex) =>
-        prevIndex === dummyData.length - 1 ? 0 : prevIndex + 1
+        prevIndex === restaurants.length - 1 ? 0 : prevIndex + 1
       );
     },
     onSwipedRight: () => {
       // Handle right swipe (e.g., like the restaurant)
       setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? dummyData.length - 1 : prevIndex - 1
+        prevIndex === 0 ? restaurants.length - 1 : prevIndex - 1
       );
     },
     // You can add more swipe event handlers as needed
   });
 
   return (
-    <Card {...handlers} style={{  width: '20rem', margin: '0 auto' }}>
-      <Card.Img variant="top" src={picture} alt={`Image of ${name}`} />
+    <Card {...handlers} style={{ width: '20rem', margin: '0 auto' }}>
+      <Card.Img variant="top" src={photoUrl} alt={`Image of ${name}`} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <Card.Text>Location: {location}</Card.Text>
-        <Card.Text>Price: {price}</Card.Text>
+        <Card.Text>{reviews}</Card.Text>
+        <Card.Text>Address: {address}</Card.Text>
+        <Card.Text>Price: {priceRange}</Card.Text>
       </Card.Body>
     </Card>
   );
