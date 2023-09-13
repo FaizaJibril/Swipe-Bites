fdd51467cac4611e7c9c77cd8d02bb9d0a012d1f
Ifunanya <ify.zubby@gmail.com>
2023-09-13 09:18:06 +0100

    Push 1

--- final/react-updated/src/components/ui/Header.js
+++ final/react-updated/src/components/ui/Header.js
@@ -15,27 +15,7 @@ const Header = () => {
 	return (
 		<>
 			<header>				
-				<Nav>
-					<Navbar bg="light" variant="light" expand="lg">
-						<Container>
-							<Navbar.Toggle aria-controls="basic-navbar-nav" />
-							<Navbar.Collapse id="basic-navbar-nav">
-								<Nav className="justify-content-center">
-									{(currentUser) &&
-										<NavDropdown title="Drop Down" id="basic-nav-dropdown">
-											<LinkContainer to="/nonexistant">
-												<NavDropdown.Item>Some Menu</NavDropdown.Item>
-											</LinkContainer>
-											<LinkContainer to="/nonexistant">
-												<NavDropdown.Item>Some Other Menu</NavDropdown.Item>
-											</LinkContainer>
-										</NavDropdown>
-									}
-								</Nav>
-							</Navbar.Collapse>
-						</Container>
-					</Navbar>
-				</Nav>
+				
 			</header>
 		</>
 	);
