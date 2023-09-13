69837cb15f78cda2955d3a101796e2ddd5df0e00
Fabio Matola <fabiom.admi@gmail.com>
2023-09-13 16:15:28 +0100

    RestaurantDao & Controller

---
+++ final/java-server/src/main/java/com/techelevator/model/Restaurant.java
@@ -0,0 +1,80 @@
+package com.techelevator.model;
+
+public class Restaurant {
+    private int id;
+    private String name;
+    private String cuisine;
+    private int priceRange;
+    private String reviews;
+    private String photoUrl;
+    private String address;
+
+
+
+    public Restaurant() {
+        this.id = id;
+        this.name = name;
+        this.cuisine = cuisine;
+        this.priceRange = priceRange;
+        this.reviews = reviews;
+        this.address = address;
+        this.photoUrl = photoUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
+    public int getPriceRange() {
+        return priceRange;
+    }
+
+    public void setPriceRange(int priceRange) {
+        this.priceRange = priceRange;
+    }
+
+    public String getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(String reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }   public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+
+}
+
