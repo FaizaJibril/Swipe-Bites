5f9b34cc2ed054c6e0e253f9024792c79be565a2
Faiza Jibril <jayfaiza04@gmail.com>
2023-09-20 15:27:47 +0100

    delete from database and book now buttons

--- final/react-updated/src/components/ui/BackgroundVideo.js
+++ final/react-updated/src/components/ui/BackgroundVideo.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import IntroVideo from './../../Video/IntroVideo.mp4';
+import './Background.css';
 
 const BackgroundVideo = () => {
   const navigate = useNavigate();
@@ -22,7 +23,7 @@ const BackgroundVideo = () => {
 
   return (
     <div className="video-container">
-      <video id="background-video" src={IntroVideo} autoPlay muted />
+      <video id="background-video" src={IntroVideo} autoPlay muted loop className="fullscreen-video"/>
     </div>
   );
 };
